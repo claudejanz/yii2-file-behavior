9b74e0f1a9553d530af747e146d87bb781d5c657
Eugene Formanenko <mo4islona@gmail.com>
2015-01-29 11:53:49 +0300

    Update FileBehavior.php

--- FileBehavior.php
+++ FileBehavior.php
@@ -1,5 +1,4 @@
 <?php
-
 /*
  * Copyright (C) 2014 Claude Janz <claude.janz@gmail.com>
  *
@@ -22,7 +21,6 @@ namespace claudejanz\fileBehavior;
 use Yii;
 use yii\base\Behavior;
 use yii\base\InvalidConfigException;
-use yii\base\Model;
 use yii\db\ActiveRecord;
 use yii\helpers\FileHelper;
 use yii\web\UploadedFile;
@@ -32,75 +30,70 @@ use yii\web\UploadedFile;
  *
  * @author Claude Janz <claude.janz@gmail.com>
  */
-class FileBehavior extends Behavior {
-
+class FileBehavior extends Behavior
+{
     /**
-     * List of attributes that will be handeled
+     * List of attributes that will be handled
      * default: get list from model rules
      * 'image'
      * ['image','image2']
-     * @var string|array 
+     * @var string|array
      */
     public $fileNameAttributes;
-
     /**
-     * Is url replacement actif. If true all urls returned to model will be modified like this:
+     * Is url replacement active. If true all urls returned to model will be modified like this:
      * @webroot -> @web and @secureroot -> @secure
      * by preg_replace '/(@[a-z]*)root/' => '$1'
-     * @var boolean 
+     * @var boolean
      */
-    public $isUrlReplacementActif = true;
+    public $isUrlReplacementActive = true;
 
     /**
      * The attribute that represents saving path.
-     * default: '@webroot/'
      * All strings between {xxx} will be replaced with model values even auto-incremented primary keys.
      * '@webroot/images/thumbnail/{id}/'=>'@web/images/thumbnail/234/'
-     * You can set default path AND/OR spécific path. Default path to key 0.
+     * You can set default path AND/OR specific path. Default path to key 0.
      * ['@webroot/images/all/{id}/','thumbnail'=>'@webroot/images/thumbnail/{id}/']
-     * @var string|array 
+     * @var string|array
      */
     public $paths = '@webroot/';
-
     /**
      * Determies if fullpath or filename will be assigned to model.
-     * default: true
-     * You can set default returnFullPath AND/OR spécific returnFullPath. Default path to key 0.
+     * You can set default returnFullPath AND/OR specific returnFullPath. Default path to key 0.
      * [true,'thumbnail'=>false]
-     * @var boolean|array 
+     * @var boolean|array
      */
     public $returnFullPath = true;
-
     /**
-     * Default value for skipOnEmpty. If field is submitted empty it keeps the old value 
+     * Default value for skipOnEmpty. If field is submitted empty it keeps the old value
      * default: true
-     * You can set default skipOnEmpty AND/OR spécific skipOnEmpty. Default path to key 0.
+     * You can set default skipOnEmpty AND/OR specific skipOnEmpty. Default path to key 0.
      * [true,'thumbnail'=>false]
      * if fileNameAttributes is not in behavior config but loaded from model rules, rules settings will be predominant
-     * @var boolean|array  
+     * @var boolean|array
      */
     public $skipOnEmpty = true;
 
     /*
-     * private vars for internal manipulations
+     * Private vars for internal manipulations
      */
-    private $configArray;
-    private $files;
-    private $oldUrls;
-    private $isActif = false;
+    private $_configArray;
+    private $_files;
+    private $_oldUrls;
+    private $_isActive = false;
 
     /**
-     * Registers Events only if loadWithFiles is called to avoid conflict with SearchModels
+     * Register Events only if loadWithFiles is called to avoid conflict with SearchModels
      * @return array event list
      */
-    public function events() {
-        /* @var $model ActiveRecord */
-        return($this->isActif) ? [
+    public function events()
+    {
+        return ($this->_isActive) ? [
             ActiveRecord::EVENT_BEFORE_VALIDATE => 'beforeValidate',
             ActiveRecord::EVENT_AFTER_VALIDATE => 'afterValidate',
             ActiveRecord::EVENT_AFTER_INSERT => 'afterInsert',
             ActiveRecord::EVENT_AFTER_UPDATE => 'afterUpdate',
-                ] : [];
+        ] : [];
     }
 
     /**
@@ -108,13 +101,14 @@ class FileBehavior extends Behavior {
      * @param array $data
      * @param string $formName
      * @see Model::load()
+     * @return boolean
      */
-    public function loadWithFiles($data, $formName = null) {
+    public function loadWithFiles($data, $formName = null)
+    {
         /* @var $model ActiveRecord */
         $model = $this->owner;
         $this->beforeLoad();
-        if ($model->load($data, $formName)) {
-            // activate listeners
+        if($model->load($data, $formName)) {
             $this->activateEvents();
             return true;
         }
@@ -122,38 +116,41 @@ class FileBehavior extends Behavior {
     }
 
     /**
-     * Registers Events
+     * Register Events
      */
-    private function activateEvents() {
+    private function activateEvents()
+    {
         /* @var $model ActiveRecord */
         $model = $this->owner;
-        $this->isActif = true;
+        $this->_isActive = true;
         $this->attach($model);
     }
 
     /**
-     * Unregisters Events
+     * Unregister Events
      */
-    private function disableEvents() {
+    private function disableEvents()
+    {
         /* @var $model ActiveRecord */
         $model = $this->owner;
-        $this->isActif = false;
+        $this->_isActive = false;
         $this->attach($model);
     }
 
     /**
      * Before load registers old file Names.
-     * Called from 
+     * Called from
      */
-    public function beforeLoad() {
+    public function beforeLoad()
+    {
         /* @var $model ActiveRecord */
         $model = $this->owner;
         //make config file
         $this->setConfig();
         // keep old urls
-        if (!$model->isNewRecord) {
-            foreach ($this->fileNameAttributes as $value) {
-                $this->oldUrls[$value] = $model->{$value};
+        if(!$model->isNewRecord) {
+            foreach($this->fileNameAttributes as $value) {
+                $this->_oldUrls[$value] = $model->{$value};
             }
         }
     }
@@ -162,16 +159,16 @@ class FileBehavior extends Behavior {
      * Before validate event.
      * Gets new file instances.
      */
-    public function beforeValidate() {
-
+    public function beforeValidate()
+    {
         /* @var $model ActiveRecord */
         $model = $this->owner;
         // get uploaded files instances
-        foreach ($this->fileNameAttributes as $value) {
-            $this->files[$value] = UploadedFile::getInstance($model, $value);
-            if ($this->files[$value]) {
-                $path = $this->resolveUrl($this->configArray[$value][0]);
-                $model->{$value} = (($this->configArray[$value][1]) ? $path : '') . $this->files[$value]->name;
+        foreach($this->fileNameAttributes as $value) {
+            $this->_files[$value] = UploadedFile::getInstance($model, $value);
+            if($this->_files[$value]) {
+                $path = $this->resolveUrl($this->_configArray[$value][0]);
+                $model->{$value} = (($this->_configArray[$value][1]) ? $path : '').$this->_files[$value]->name;
             }
         }
     }
@@ -180,16 +177,14 @@ class FileBehavior extends Behavior {
      * After validate event
      * Assign old value if skipOnEmpty and new value empty
      */
-    public function afterValidate() {
+    public function afterValidate()
+    {
         /* @var $model ActiveRecord */
         $model = $this->owner;
-        if (!$model->isNewRecord) {
-            foreach ($this->fileNameAttributes as $value) {
-                if($model->{$value.'_delete'}==1 && !$this->files[$value]){
-                    $oldFilePath = Yii::getAlias(str_replace('@web', '@webroot', $this->oldUrls[$value]));
-                    if(is_file($oldFilePath))unlink($oldFilePath);
-                }elseif (empty($model->{$value}) && !empty($this->oldUrls[$value]) && $this->skipOnEmpty[$value]) {
-                    $model->{$value} = $this->oldUrls[$value];
+        if(!$model->isNewRecord) {
+            foreach($this->fileNameAttributes as $value) {
+                if(empty($model->{$value}) && !empty($this->_oldUrls[$value]) && $this->skipOnEmpty[$value]) {
+                    $model->{$value} = $this->_oldUrls[$value];
                 }
             }
         }
@@ -198,7 +193,8 @@ class FileBehavior extends Behavior {
     /**
      * After insert event
      */
-    public function afterInsert() {
+    public function afterInsert()
+    {
         $this->afterSave();
         $this->updateDbOnInsert();
     }
@@ -206,113 +202,119 @@ class FileBehavior extends Behavior {
     /**
      * After update event
      */
-    public function afterUpdate() {
+    public function afterUpdate()
+    {
         $this->afterSave();
     }
 
     /**
      * Saves files to definitive position
      */
-    public function afterSave() {
+    public function afterSave()
+    {
         // last listener has been call so we can disable event
         $this->disableEvents();
-        $model = $this->owner;
-       
-        foreach ($this->fileNameAttributes as $value) {
-            if ($this->files[$value]) {
-                $path = $this->resolvePath($this->configArray[$value][0]);
+
+        foreach($this->fileNameAttributes as $value) {
+
+            if($this->_files[$value]) {
+                $path = $this->resolvePath($this->_configArray[$value][0]);
                 $folder = Yii::getAlias($path);
-                if (!is_dir($folder)) {
-                    FileHelper::createDirectory($folder);
+
+                if(!is_dir($folder)) {
+                    if(!FileHelper::createDirectory($folder)) {
+                        throw new InvalidConfigException("The directory is not writable by the Web process: {$folder}");
+                    }
                 }
-                $this->files[$value]->saveAs($folder . $this->files[$value]->name);
+                $this->_files[$value]->saveAs($folder.$this->_files[$value]->name);
             }
         }
     }
 
     /**
-     * replaces '/(@[a-z]*)root/' => '$1' if isUrlReplacementActif
+     * Replaces '/(@[a-z]*)root/' => '$1' if isUrlReplacementActive
      * @param string $path
      * @return string
      */
-    public function resolveUrl($path) {
-
+    public function resolveUrl($path)
+    {
         $path = $this->resolvePath($path);
-        return ($this->isUrlReplacementActif) ? preg_replace('/(@[a-z]*)root/', '$1', $path) : $path;
+
+        return ($this->isUrlReplacementActive) ? preg_replace('/(@[a-z]*)root/', '$1', $path) : $path;
     }
 
     /**
-     * adds varibables to path
+     * Adds variables to path
      * @param string $path
      * @return string
      */
-    public function resolvePath($path) {
+    public function resolvePath($path)
+    {
         $model = $this->owner;
-        return preg_replace_callback(
-                '|{(.*?)}|', function ($matches) use ($model) {
+
+        return preg_replace_callback('|{(.*?)}|', function ($matches) use ($model) {
             return ($model->{$matches[1]}) ? $model->{$matches[1]} : $matches[0];
-        }, $path
-        );
+        }, $path);
     }
 
     /**
      * Makes initial config for file upload
      */
-    public function setConfig() {
+    public function setConfig()
+    {
         $model = $this->owner;
         /* $this->skipOnEmpty is booleans cast as array */
-        if (is_bool($this->skipOnEmpty)) {
+        if(is_bool($this->skipOnEmpty)) {
             $this->skipOnEmpty = (array) $this->skipOnEmpty;
         }
 
         /* $this->fileNameAttributes is string cast as array */
-        if (is_string($this->fileNameAttributes)) {
+        if(is_string($this->fileNameAttributes)) {
             $this->fileNameAttributes = (array) $this->fileNameAttributes;
         }
         /* if fileNameAttributes is not set try to get it from rules */
-        if (!isset($this->fileNameAttributes)) {
-            foreach ($model->rules() as $rule) {
-                if ($rule[1] == 'file') {
-
+        if(!isset($this->fileNameAttributes)) {
+            foreach($model->rules() as $rule) {
+                if($rule[1] == 'file') {
                     $attributes = (array) $rule[0];
                     $this->fileNameAttributes = array_merge((array) $this->fileNameAttributes, $attributes);
-                    foreach ($attributes as $value) {
+                    foreach($attributes as $value) {
                         $this->skipOnEmpty[$value] = (isset($rule['skipOnEmpty'])) ? $rule['skipOnEmpty'] : $this->skipOnEmpty[0];
                     }
                 }
             }
         }
         /* if still no fileNameAttributes throw an error */
-        if (!isset($this->fileNameAttributes)) {
+        if(!isset($this->fileNameAttributes)) {
             throw new InvalidConfigException(Yii::t('app', 'The "fileNameAttributes" must be set. No file validation rules() found in your model. like: [[\'image\',\'image2\'], \'file\', \'extensions\' => \'jpg\'] or set it manually for "{behavior}"', ['behavior' => __CLASS__]));
         }
 
         /* if $this->paths is string cast as array */
-        if (is_string($this->paths)) {
+        if(is_string($this->paths)) {
             $this->paths = (array) $this->paths;
         }
 
         /* $this->returnFullPath is boolean cast as array */
-        if (is_bool($this->returnFullPath)) {
+        if(is_bool($this->returnFullPath)) {
             $this->returnFullPath = (array) $this->returnFullPath;
         }
 
         /**
          *  make config array
          */
-        $this->configArray = [];
+        $this->_configArray = [];
 
-        foreach ($this->fileNameAttributes as $value) {
+        foreach($this->fileNameAttributes as $value) {
             /* set foreach $this->fileNameAttributes a skipOnEmpty */
-            if (!isset($this->skipOnEmpty[$value])) {
-                if (!isset($this->skipOnEmpty[0])) {
+            if(!isset($this->skipOnEmpty[$value])) {
+                if(!isset($this->skipOnEmpty[0])) {
                     throw new InvalidConfigException(Yii::t('app', 'Missing "skipOnEmpty" for "{item}" in {behavior} and no default "skipOnEmpty" is set.', ['item' => $value, 'behavior' => __CLASS__]));
                 }
                 $this->skipOnEmpty[$value] = $this->skipOnEmpty[0];
             }
             /* set foreach $this->fileNameAttributes a returnFullPath */
-            if (!isset($this->returnFullPath[$value])) {
-                if (!isset($this->returnFullPath[0])) {
+            if(!isset($this->returnFullPath[$value])) {
+                if(!isset($this->returnFullPath[0])) {
                     throw new InvalidConfigException(Yii::t('app', 'Missing "returnFullPath" for "{item}" in {behavior} and no default "returnFullPath" is set.', ['item' => $value, 'behavior' => __CLASS__]));
                 }
                 $this->returnFullPath[$value] = $this->returnFullPath[0];
@@ -321,13 +323,13 @@ class FileBehavior extends Behavior {
              * set foreach $this->fileNameAttributes a path
              * 
              */
-            if (!isset($this->paths[$value])) {
+            if(!isset($this->paths[$value])) {
                 /*
                  * for $value -> image 
                  * $paths -> ['image'=>'@webroot/images/','thumbnail'=>'@webroot/thumb/','@webroot/default/']
                  * returns '@webroot/images/'
                  */
-                if (!isset($this->paths[0])) {
+                if(!isset($this->paths[0])) {
                     throw new InvalidConfigException(Yii::t('app', 'Missing path for "{item}" in {behavior} and no default path is set.', ['item' => $value, 'behavior' => __CLASS__]));
                     /*
                      * for $value -> image 
@@ -341,47 +343,51 @@ class FileBehavior extends Behavior {
              * set foreach $this->fileNameAttributes a path
              * $key=>[path,returnFullPath,skipOnEmpty] 
              */
-            $this->configArray[$value] = [$this->paths[$value], $this->returnFullPath[$value], $this->skipOnEmpty[$value]];
+            $this->_configArray[$value] = [
+                $this->paths[$value],
+                $this->returnFullPath[$value],
+                $this->skipOnEmpty[$value]
+            ];
         }
     }
 
     /**
      * update db entry after save if necessary (pk replacement)
      */
-    private function updateDbOnInsert() {
+    private function updateDbOnInsert()
+    {
         /* @var $model ActiveRecord */
         $model = $this->owner;
 
         // update model and db after insert for autoincrement PK in file url
         $schema = $model->getTableSchema();
-        $modifications;
-        foreach ($this->fileNameAttributes as $value) {
-            if ($this->files[$value]) {
+        $modifications = [];
+        foreach($this->fileNameAttributes as $value) {
+            if($this->_files[$value]) {
                 // get new value
-                $newValue = $this->resolveUrl($this->configArray[$value][0]) . $this->files[$value]->name;
+                $newValue = $this->resolveUrl($this->_configArray[$value][0]).$this->_files[$value]->name;
 
-                if ($model->{$value} != $newValue) {
+                if($model->{$value} != $newValue) {
                     // if new value set model
                     $model->{$value} = $newValue;
-                    if ($schema->getColumn($value)) {
+                    if($schema->getColumn($value)) {
                         // if in schema column add to modifications for db update
                         $modifications[$value] = $newValue;
                     }
                 }
             }
         }
-        if (isset($modifications)) {
+        if(isset($modifications)) {
             // if modifications update db
             $db = $model->getDb();
             $where = '';
-            foreach ($model->getPrimaryKey(true) as $k => $v) {
-                if (!empty($where)) {
+            foreach($model->getPrimaryKey(true) as $k => $v) {
+                if(!empty($where)) {
                     $where .= ' AND ';
                 }
-                $where.=$k . '=' . $v;
+                $where .= $k.'='.$v;
             }
             $db->createCommand()->update($schema->name, $modifications, $where)->execute();
         }
     }
-
 }
